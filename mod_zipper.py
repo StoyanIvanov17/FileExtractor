7b0a031ffe33583ffd5f89ac458d6d24426ae05a
Stoyan Ivanov <stoyanivanovmail@gmail.com>
2024-07-24 16:22:16 +0300

    Update mod_zipper.py

--- mod_zipper.py
+++ mod_zipper.py
@@ -11,12 +11,11 @@ directory = 'D:\\World_of_Tanks_EU\\mods\\'
 
 # Gets the newest added folder in the selected mod directory. For example:
 # The game adds a new update every week and the mod has to be configured to the current game version to work.
-# That is why it is required to select the last folder in a directory.
+# Hence why it's required to select the latest folder in the directory.
 newest_patch = max(glob.glob(os.path.join(directory, '*/')), key=os.path.getmtime)
 
-# We iterate over the selected path above (where we download or store our zip files)
-# As long as we have only one (the zip file we want to use) in that folder we do not need to specify it's name every time
-# This way we can freely run the script any time we want without any corrections.
+# We iterate over the selected path above (where we download or store our zip files).
+# As long as we have only one (the zip file we want to use) in that folder we do not need to specify its name each time we run the script.
 for f in p.glob('*.zip'):
     with zipfile.ZipFile(f, 'r') as archive:
         archive.extractall(path=newest_patch)
